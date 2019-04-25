8a4909174e9d913476705afea1cdf15f63e71970
Jeroen Ooms <jeroenooms@gmail.com>
2019-04-25 11:31:36 +0200

    tweaks

--- build.R
+++ build.R
@@ -1,5 +1,9 @@
+# Fixed settings
+options(menu.graphics = FALSE)
+doc_dir <- "/data/docs/"
+src_dir <- "/data/src/"
+
 # Firt argument should be the URL
-options(menu.graphics=FALSE)
 remote <- commandArgs(TRUE)[1]
 if(is.na(remote))
   stop("No remote parameter set")
@@ -7,24 +11,31 @@ if(is.na(remote))
 # Clone the URL and change dir
 src <- tempfile()
 gert::git_clone(remote, src)
+setwd(src)
 #remotes::install_deps(tmp, dependencies = TRUE)
-remotes::install_local(src, dependencies = TRUE, force = TRUE)
+remotes::install_local(dependencies = TRUE, force = TRUE)
+
+if(!file.exists('DESCRIPTION'))
+  stop("Not an R package")
 
 # Create source pkg
-pkgfile <- pkgbuild::build(src, dest_path = tempdir())
+pkgfile <- pkgbuild::build(dest_path = tempdir())
 pkg <- strsplit(basename(pkgfile), "_", fixed = TRUE)[[1]][1]
 
 # Each pkg
 title <- sprintf("rOpenSci: %s", pkg)
 url <- sprintf("https://docs.ropensci.org/%s", pkg)
-dest <- sprintf("/data/docs/%s", pkg)
+dest <- paste0(doc_dir, pkg)
 tmp <- paste0(dest, "_TMP")
 template <- list(package = "rotemplate")
 unlink(tmp, recursive = TRUE)
-pkgdown::build_site(src, document = FALSE, preview = FALSE, override = 
+pkgdown::build_site(document = FALSE, preview = FALSE, override =
   list(destination = tmp, title = title, url = url, template = template))
+unlink(dest, recursive = TRUE)
 file.rename(tmp, dest)
 
-# Store the source pkg
-dir.create("/data/src/", showWarnings = FALSE)
-file.copy(pkgfile, "/data/src/", overwrite = TRUE)
+# Store the source pkg and update repo
+dir.create(src_dir, showWarnings = FALSE)
+unlink(sprintf("%s%s_*.tar.gz", src_dir, pkg))
+file.copy(pkgfile, src_dir)
+tools::write_PACKAGES(src_dir)
