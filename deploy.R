21f0e58b744d850648032245b5bd4890d7168277
Jeroen Ooms <jeroenooms@gmail.com>
2019-06-04 18:23:02 +0200

    Fixes for changes in betty

--- deploy.R
+++ deploy.R
@@ -1,6 +1,5 @@
 # Container options
 deploy_org = "ropensci-docs"
-site_root = "https://docs.ropensci.org"
 doc_root = "/data/docs"
 
 # Firt argument should be the URL
@@ -8,9 +7,8 @@ library(betty)
 args <- commandArgs(TRUE)
 package <- args[1]
 if(is.na(package)){
-	deploy_all_sites(doc_root = doc_root, deploy_org = deploy_org, site_root = site_root)
+	deploy_all_sites(doc_root = doc_root, deploy_org = deploy_org)
 } else {
 	path <- paste0(doc_root, "/", package)
-	deploy_url <- paste0(site_root, "/", package)
-	deploy_site(path = path, deploy_org = deploy_org, deploy_url = deploy_url)
+	deploy_site(path = path, deploy_org = deploy_org)
 }
