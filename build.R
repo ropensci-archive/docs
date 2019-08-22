7961a78583307825b807d0b60f09ff7b1917623b
Jeroen Ooms <jeroenooms@gmail.com>
2019-08-22 13:58:26 +0200

    pass git url in docs

--- build.R
+++ build.R
@@ -2,8 +2,9 @@
 library(betty)
 args <- commandArgs(TRUE)
 repo <- args[1]
+git_url <- args[2]
 if(is.na(repo)){
   build_all_sites(dest = "/data")
 } else {
-  build_site(repo, dest = "/data")
+  build_site(repo, dest = "/data", git_url = git_url)
 }
