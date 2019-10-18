e972252c47c00824e082b56ff60db872743df535
Jeroen Ooms <jeroenooms@gmail.com>
2019-10-18 12:26:19 +0200

    Might as well use default

--- universe.R
+++ universe.R
@@ -1,7 +1,5 @@
 # Firt argument should be the URL
 library(betty)
 args <- as.list(commandArgs(TRUE))
-# This can cause race conditions right now
-#args <- c(args, dest = "/data")
-args <- c(args, dest = tempdir())
+#args <- c(args, dest = "/data") # Causes race conditions between packages on Jenkins
 do.call(betty::update_universe, args)
