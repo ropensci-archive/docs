48c0824ff32d1ae7f2db6317cb267cb7129171da
Jeroen Ooms <jeroenooms@gmail.com>
2019-10-23 14:52:17 +0200

    workaround for pandoc 99 error

--- setup.R
+++ setup.R
@@ -4,3 +4,6 @@ options(menu.graphics = FALSE, repos = "https://cloud.r-project.org")
 # Many packages use devtools in their README.rmd
 install.packages(c("devtools", "testthat"))
 remotes::install_github("jeroen/betty")
+
+# Fixes https://github.com/rstudio/rmarkdown/issues/1667
+remotes::install_github("rstudio/rmarkdown")
