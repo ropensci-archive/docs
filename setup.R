2ce20fe7e27d657520308d7ec3d68206e97b35aa
Jeroen Ooms <jeroenooms@gmail.com>
2019-11-28 14:25:57 +0100

    new rmarkdown is on cran

--- setup.R
+++ setup.R
@@ -4,6 +4,3 @@ options(menu.graphics = FALSE, repos = "https://cloud.r-project.org")
 # Many packages use devtools in their README.rmd
 install.packages(c("devtools", "testthat"))
 remotes::install_github("jeroen/betty")
-
-# Fixes https://github.com/rstudio/rmarkdown/issues/1667
-remotes::install_github("rstudio/rmarkdown")
